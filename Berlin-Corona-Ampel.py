19bb8f26bba9f3bd915dbd4589838ae1550fb6f0
thomasstolt <thomas.stolt@gmail.com>
2021-11-07 20:33:15 +0100

    First round finished and working, need to do logging

--- Berlin-Corona-Ampel.py
+++ Berlin-Corona-Ampel.py
@@ -1,8 +1,26 @@
-from phue import Bridge
+# =========== #
+# DESCRIPTION #
+# =========== #
+
+# This program will retrieve the latest Corona status indicators for Berlin and control
+# 3 Philips Hue lights (in our case Philips Hue Play) to represent the corona traffic light
+# of the Berlin authorities. There are currently (Nov 2021) three pandemic indicators:
+# 1. 7-Tage-Inzidenz: all infections of the last 7 days per 100.000 inhabitants
+# 2. 7-Tage-Hospitalisierungs-Inzidenz: all hospitalisations of the last 7 days across 100.000 inhabitants
+# 3. ITS-Belegung: percentage of intensive care beds occupied by infected patients
+# https://www.berlin.de/corona/lagebericht/
+
+
+# IMPORTS
+# =======
+
 import json
 import requests
-from datetime import date, timedelta, datetime  # Time is important
 import logging
+from requests.models import DEFAULT_REDIRECT_LIMIT
+from phue import Bridge
+from datetime import date, timedelta, datetime  # Time is important
+import time
 
 # DEFINITIONS
 # ===========
@@ -12,49 +30,88 @@ import logging
 # you can try to leave it as it is, in most cases this should work.
 HUE_BRIDGE_IP = "Philips-hue"
 
-# Put your Philips Hue API Key for your Bridge into a text file and specify the location here
+# Put your "Philips Hue API" Key for your Bridge into the text file named below.
 # To create an API Key, you need to do the following:
 # While in your home network go to http://Philips-hue/debug/clip.html and enter in the fields:
 # URL: /api
 # Message Body: {"devicetype":"my_hue_app#my_hue_user"}
-# where 'my_hue_app' is an arbitrary application name and my_hue_user is a user name of your
-# chosing, seperated by '#'. If you don't know what all this means: leave it as it is! :)
-# This will return your API (a long Character-Symbol-Number-String) key which you can now
-# use to connect to the Bridge.'''
+# where 'my_hue_app' is an arbitrary application name and 'my_hue_user' is a user name of your
+# chosing, both seperated by '#'. If you don't know what all this means: leave it as it is! :)
+# This will return your API (a long Character-Symbol-Number-String) key which we will use to
+# connect to the Bridge and control the lights.
 API_KEY_FILE_NAME = "Philips_Hue_API_Key.txt"
+
+# Fetch the API key from the file defined above
+keyfile = open(API_KEY_FILE_NAME,'r')
+API_KEY = keyfile.read()
+keyfile.close()
+
 
 # URL of the data
 DATA_URL = 'https://www.berlin.de/lageso/gesundheit/infektionskrankheiten/corona/tabelle-indikatoren-gesamtuebersicht/index.php/index/all.json'
 
-# This function returns the first value in a list that is non-NULL
-def first_none_null(iterable, func=lambda L: L is not None, **kwargs):
-    it = (el for el in iterable if func(el))
-    if 'default' in kwargs:
-        return next(it, kwargs[default])
-    return next(it) # no default so raise 'StopIteration'
+
+# FUNCTIONS
+# =========
+
+# Set a given hue light to colour
+def set_hue_green(light_nr, colour):
+    headers = { 'Accept': 'application/json' }
+    if   colour == "green":  data = '{"on":true,"xy":[0.2083,0.6713],"bri":60}'
+    elif colour == "yellow": data = '{"on":true,"xy":[0.4682,0.4760],"bri":60}'
+    else:                    data = '{"on":true,"xy":[0.6750,0.3220],"bri":60}'
+    requests.put("http://" + HUE_BRIDGE_IP + "/api/"  + API_KEY + "/lights/" + str(light_nr) + "/state", headers=headers, data=data)
+
+def set_hue_off(light_nr):
+    headers = { 'Accept': 'application/json' }
+    data = '{"on":false}'
+    requests.put("http://" + HUE_BRIDGE_IP + "/api/"  + API_KEY + "/lights/" + str(light_nr) + "/state", headers=headers, data=data)
 
 
+# ============= #
+# MAIN PROGRAMM #
+# ============= #
+
+# Switch all lights off for indication
+set_hue_off(13)
+set_hue_off(14)
+set_hue_off(15)
+time.sleep(1)
+
+# Get the current data first and load into a dictionary
 jsonStr = requests.get(DATA_URL).text
 my_dict = json.loads(jsonStr)
 
-for i in my_dict['index']:
-    print(i['id'],i['datum'],i['fallzahl'],i['neue_faelle'],i['genesene'],i['todesfaelle'],i['7_tage_inzidenz'],i['rel_veraenderung_der_7_tage_inzidenz'],i['7_tage_hosp_inzidenz'],i['its_belegung'])
+# 7-Tage-Inzidenz
+# Find most recent "None Null" value for "7_tage_inzidenz"
+i = 0
+while my_dict['index'][i]['7_tage_inzidenz'] is None: i+=1
+# print("Letzte 7-Tage-Inzidenz: ", my_dict['index'][i]['7_tage_inzidenz'])
+# Set the color to green, yellow or red accordingly
+if         int(float(my_dict['index'][i]['7_tage_inzidenz'])) < 35:    set_hue_green(13, "green")
+elif 35 <= int(float(my_dict['index'][i]['7_tage_inzidenz'])) < 100:   set_hue_green(13, "yellow")
+elif       int(float(my_dict['index'][i]['7_tage_inzidenz'])) >= 100:  set_hue_green(13, "red")
 
+# 7-Tage-Hospitalisierungs Inzidenz
+# Find most recent "None Null" value for "7_tage_hosp_inzidenz"
+i = 0
+while my_dict['index'][i]['7_tage_hosp_inzidenz'] is None: i+=1
+# print("7-Tage-Hospitalisierungs Inzidenz: ", my_dict['index'][i]['7_tage_hosp_inzidenz'])
+# Set the color to green, yellow or red accordingly
+if int(float(my_dict['index'][i]['7_tage_hosp_inzidenz'])) < 4:        set_hue_green(14, "green")
+elif 4 <= int(float(my_dict['index'][i]['7_tage_hosp_inzidenz'])) < 8: set_hue_green(14, "yellow")
+elif int(float(my_dict['index'][i]['7_tage_hosp_inzidenz'])) >= 8:     set_hue_green(14, "red")
 
-print(type(my_dict))
-print(list(my_dict.values())[3])
+# ITS Belegung
+# Find most recent "None Null" value for "its_belegung"
+i = 0
+while my_dict['index'][i]['its_belegung'] is None: i+=1
+# print("Letzte ITS Belegung: ", my_dict['index'][i]['its_belegung'])
+# Set the color to green, yellow or red accordingly
+if        int(float(my_dict['index'][i]['its_belegung'])) < 5:         set_hue_green(15, "green")
+elif 5 <= int(float(my_dict['index'][i]['its_belegung'])) < 20:        set_hue_green(15, "yellow")
+elif      int(float(my_dict['index'][i]['its_belegung'])) >= 20:       set_hue_green(15, "red")
 
-
-fval = first_none_null(my_dict.id[0])
-print(fval)
-exit(0)
-
-
-
-
-keyfile = open(API_KEY_FILE_NAME,'r')
-API_KEY = keyfile.read()
-keyfile.close()
 
 exit(0)
 
